19d1e1fcd075f3e4470d987b8bb48f2b61cc9e8b
SpykSpartan <107502836+SpykSpartan@users.noreply.github.com>
2026-04-09 21:58:41 -0400

    all final changes

--- SEGDpro/Assets/Script/NPC.cs
+++ SEGDpro/Assets/Script/NPC.cs
@@ -5,14 +5,23 @@ public class NPC : Interactable
     public float minStress = -10f;
     public float maxStress = 15f;
 
+    public AudioClip talkSFX;
+    private AudioSource audioSource;
+
     void Start()
     {
         promptText = "Talk";
+        audioSource = GetComponent<AudioSource>();
     }
 
     public override void Interact()
     {
         float result = Random.Range(minStress, maxStress);
         StressManager.Instance.AddStress(result);
+
+        if (talkSFX != null && audioSource != null)
+        {
+            audioSource.PlayOneShot(talkSFX);
+        }
     }
 }
